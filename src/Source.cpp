c9c2837712c22d138698036d5644c8ce41cab919
GeorgeMikhailCoder <george.mikhail@yandex.ru>
2024-04-06 23:28:25 +0300

    Delete function without check data

--- src/Source.cpp
+++ src/Source.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-EqResult solve_equation1(double a, double b, double c, unsigned short eps_power)
+EqResult solve_equation(double a, double b, double c, unsigned short eps_power)
 {
     double eps = 1/pow(10,eps_power);
     double D = b*b - 4*a*c;
@@ -41,39 +41,5 @@ EqResult solve_equation1(double a, double b, double c, unsigned short eps_power)
     }
 
     res.success = std::isfinite(res.x1) && std::isfinite(res.x2);
-    return res;
-}
-
-EqResult solve_equation(double a, double b, double c, unsigned short eps_power)
-{
-    double eps = 1/pow(10,eps_power);
-    double D = b*b - 4*a*c;
-    
-    EqResult res;
-    res.tolerance = eps;
-
-
-    if (D==0) // case D == 0
-    {
-        res.nRoots=1;
-        res.x1 = -b/2*a;
-        res.x2 = 0;
-    }
-    
-    if (D > 0) // case D > 0
-    {
-        res.nRoots=2;
-        res.x1 = (-b + sqrt(D))/2*a;
-        res.x2 = (-b - sqrt(D))/2*a;
-    }
-
-    if (D < 0) // case D < 0
-    {
-        res.nRoots=0;
-        res.x1 = 0;
-        res.x2 = 0;
-    }
-
-    res.success = true;
     return res;
 }
